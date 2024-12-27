2944c2cfe77ff06f65c4e3261193195817aa311b
SHIVALEELAREDDY <itsshivureddy956@gmail.com>
2024-12-27 20:23:46 +0530

    files

---
+++ index.js
@@ -0,0 +1,19 @@
+let card=document.getElementById("card")
+card.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ZQi7L-v8AZGLyTkr7tWULn10RHI2OfCD8g&s"> 
+<h1>royal enfield</h1>
+<p>price:250000</p>
+<h4>Hero PLEASURE+ royal enfield Cast Poster black Booking for Ex-Showroom Price</h4>
+<button>Order Now</button>`
+
+let card1=document.getElementById("card1")
+card1.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSue5lWG9qo0w4BHOrnl0qkhl6ZZXUWdC8V8g&s" width="200" height="200"> 
+<h1>iphone </h1>
+<p>price:150000</p>
+<h4>iphone (black, 6GB RAM, 128GB Storage) | Global Debut SD 4s Gen 5 </h4>
+<button>Order Now</button>`
+
+
+
+
+
+
